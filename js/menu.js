2491399065d6110c523da366c40b4a236b3a0e43
gustavoquinalha <gustavo_quinalha@hotmail.com>
2017-04-29 23:14:16 -0300

    -q

---
+++ js/menu.js
@@ -0,0 +1,20 @@
+  // ABRIR MENU
+  abreMenu.onclick = function() {
+    var abre = document.getElementById('painelResp');
+    if (abre.style.display !== 'block') {
+      abre.style.display = 'block';
+    } else {
+      abre.style.display = 'none';
+    }
+  };
+  // /ABRIR MENU
+  // FECHAR MENU
+  fechaMenu.onclick = function() {
+    var fecha = document.getElementById('painelResp');
+    if (fecha.style.display !== 'block') {
+      fecha.style.display = 'block';
+    } else {
+      fecha.style.display = 'none';
+    }
+  };
+  // /FECHAR MENU
